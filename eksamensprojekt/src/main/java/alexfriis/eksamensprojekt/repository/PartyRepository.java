2774de88dc5749da325dcb3402be9591a8bd1fd5
AlexFriis <70746933+AlexFriis@users.noreply.github.com>
2021-12-21 02:35:55 +0100

    Delete eksamensprojekt directory

--- eksamensprojekt/src/main/java/alexfriis/eksamensprojekt/repository/PartyRepository.java
+++
@@ -1,9 +0,0 @@
-package alexfriis.eksamensprojekt.repository;
-
-import alexfriis.eksamensprojekt.entity.Party;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartyRepository extends JpaRepository<Party, Long> {
-}
